180bcc6c3a568cf24b0aa89bd76819754615fbd9
lucas-lira30 <pontocom9190@gmail.com>
2024-05-13 08:55:10 -0300

    Aula de netbeans

---
+++ NetBeans/Aluno/src/aluno/Aluno.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aluno;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Aluno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AlunoObj alunol;
+        
+        alunol = new AlunoObj ();
+        
+        alunol.setNome ("Lucas Lira");
+        System.out.println(alunol.getnome());
+        
+        
+        alunol.setIdade (16);
+        System.out.println(alunol.getIdade());
+                
+    }
+    public int getIdade (){
+    
+return idade;
+    }
